c5136fa624321a86c8de539a4edd4a7fc05aecba
YING LIANG <y.liang.168@westcliff.edu>
2023-04-22 23:53:36 -0500

    input validation added

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const path = require("path");
 const basic = auth.basic({
   file: path.join(__dirname,"./users.htpasswd"),
 });
+const mongoSanitize = require("express-mongo-sanitize");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const {check, validationResult} = require("express-validator");
 
 const welcomeContent = "Hello! Welcome To Web Development Bootcamp Family! If you do not alreay have a profile, please register now and start to log your web development journey!";
 const aboutContent = "Prepare for an exciting career as a full stack web developer with a Coding Bootcamp Certificate from Westcliff University. Students learn today’s cutting-edge web development technologies taught by Westcliff’s professors. The program offers a fully immersive live online learning experience where students will gain proficiency in front end and back end web development technologies: HTML5, CSS3, Javascript ES6, Git, Github, MongoDB, Express, ReactJS, NodeJS, etc.";
@@ -20,16 +25,30 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.use(mongoSanitize());
+app.use(xss());
+app.use(helmet());
 
 app.use(session({
   secret:"our little secret.",
   resave:false,
   saveUninitialized:false,
+  httpOnly:true,
   cookie:{
     secure:false,
     maxAge:60000  //60s
   }
 }))
+
+const limit = rateLimit({
+  max:100,
+  windowMs:60*60*1000,
+  message:"Too many requests"
+});
+
+app.use('/users',limit);
+
+app.use(express.json({limit:'10kb'}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -72,16 +91,27 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.post("/register", function(req, res){
 
-  User.register({username:req.body.username,bio:req.body.bio},req.body.password, function(err,user){
-    if(err){
-      console.log(err);
-    }else{
-      passport.authenticate('local')(req, res, function(){
-        res.redirect('/users');})
-    }
-  })
+app.post("/register", 
+[
+  check('username').isLength({min:1}).withMessage('Please enter your name'),
+],
+
+function(req,res){
+  const errors=validationResult(req);
+  console.log(errors);
+  if(errors.isEmpty()) {
+    User.register({username:req.body.username,bio:req.body.bio},req.body.password, function(err,user){
+      if(err){
+        console.log(err);
+      }else{
+        passport.authenticate('local')(req, res, function(){
+          res.redirect('/users');})
+      }
+    })
+}else{
+  res.redirect('/register')
+}
 });
 
 
